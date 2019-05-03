22651b7a3155f542712a836b09f0bace407fa154
manishparmar5751 <manishparmar57@gmail.com>
2019-05-03 19:34:44 +0000

    updated packages for Quazi node REq.txt

--- install-website.sh
+++ install-website.sh
@@ -1,12 +1,15 @@
-#!/bin/sh 
-sudo yum install -y epel-release
-sudo yum install -y nginx
-sudo chkconfig nginx on
-LOCATION_OF_INDEX=/usr/share/nginx/html/index.html
-sudo bash -c "cat <<A_VERY_SPECIAL_MESSAGE>$LOCATION_OF_INDEX
-<html>
-<h2> Hello, World! </h2>
-</html> 
+#!/bin/bash -e
+sudo apt-get update 
+sudo apt-get install -y python-opencv 
+sudo apt install -y nginx  
+sudo apt-get install -y unicorn
+sudo apt-get install -y curl gnupg build-essential
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+pip install -r req.txt
+# sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# curl -sSL https://get.rvm.io | sudo bash -s stable
+# sudo usermod -a -G rvm `whoami`
+# if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
 
-A_VERY_SPECIAL_MESSAGE
-"
+
