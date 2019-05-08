7053a7a3db572a1d77adcee301201b8c65a24838
Gopalasivam <gopalasivam.palaniappan@eplus.com>
2019-05-08 12:32:22 -0700

    Added solution for installing the Multiple versions of Ruby using RVM and Rails with gemsets without VM reboot during Image building process

--- install-website.sh
+++ install-website.sh
@@ -5,7 +5,20 @@ sudo apt install -y nginx
 sudo apt-get install -y unicorn
 sudo apt-get install -y curl gnupg build-essential
 sudo apt-get install -y python-pip python-dev
-pip install -r ./quazi/req.txt
+sudo apt-get install -y software-properties-common
+
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt update
+sudo apt install -y rvm
+echo '[[ -s "/usr/share/rvm/scripts/rvm" ]] && . "/usr/share/rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+source ~/.bash_profile
+rvm fix-permissions
+rvmsudo rvm install 2.2.2
+rvmsudo rvm install 2.5.3
+sudo gem install rails -v 4.1.0
+
+#pip install -r ./quazi/req.txt
+
 # pip install --upgrade pip
 # sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 # curl -sSL https://get.rvm.io | sudo bash -s stable
